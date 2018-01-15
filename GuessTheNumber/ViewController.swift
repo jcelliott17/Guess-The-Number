b4e711e94900342f63a4c27f8fd255a9b0aa63c9
Jacqueline Elliott <jcelliott17@gmail.com>
2018-01-15 15:35:32 -0500

    Initial Commit

---
+++ GuessTheNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessTheNumber
+//
+//  Created by Jackie Elliott on 1/15/18.
+//  Copyright © 2018 GT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
